c983fc025d0038428597ba1c30d0b2392d33e8df
Shaifali Jangra <shaifali.jangra@Shaifalis-MacBook-Pro.local>
2022-04-09 16:19:02 +0530

    sidj: add the basic config

--- client/contexts/server-configs.tsx
+++ client/contexts/server-configs.tsx
@@ -1,4 +1,8 @@
 import { createContext } from "react";
+import getConfig from 'next/config'
+
+const { publicRuntimeConfig } = getConfig()
+const { baseUrl } = publicRuntimeConfig
 
 interface ServerConfigsContextType {
   serverConfigs: {
@@ -8,9 +12,11 @@ interface ServerConfigsContextType {
 
 export const data = {
   serverConfigs: {
-    baseApiPath: 'http://localhost:3001/api/v1',
+    baseApiPath: `http://${baseUrl}/api/v1`,
   }
 }
+
+console.log(baseUrl)
 
 const ServerConfigsContext = createContext<ServerConfigsContextType>(data)
 export default ServerConfigsContext;
--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    baseUrl: !!process.env.AWS_DOMAIN ? process.env.AWS_DOMAIN : 'localhost:3000'
+  }
 }
 
 module.exports = nextConfig
